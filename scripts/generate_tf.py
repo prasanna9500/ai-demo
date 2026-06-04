7570df2b2056a0afe64b027037bd4e86dd7d46f5
prasanna9500 <73993722+prasanna9500@users.noreply.github.com>
2026-06-04 17:10:09 +0530

    Update generate_tf.py

--- scripts/generate_tf.py
+++ scripts/generate_tf.py
@@ -1,1 +1,34 @@
+import os
+import google.generativeai as genai
 
+# Read Gemini API Key
+genai.configure(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+# Gemini Model
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+# Read prompt from GitHub workflow
+prompt = os.getenv("USER_PROMPT")
+
+# Ask Gemini to generate Terraform
+response = model.generate_content(
+    f"""
+Generate Terraform code only.
+
+Request:
+{prompt}
+
+Output only Terraform HCL code.
+"""
+)
+
+# Create terraform folder if needed
+os.makedirs("terraform", exist_ok=True)
+
+# Save Terraform code
+with open("terraform/main.tf", "w") as f:
+    f.write(response.text)
+
+print("Terraform file generated")
